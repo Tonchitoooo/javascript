91ea1e27bda3dc0b59644f7712aca90636cd2ef4
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-09 19:52:20 +0300

    Update howlong.js

--- parag4/04_11_06_date/howlong.js
+++ parag4/04_11_06_date/howlong.js
@@ -1,11 +1,4 @@
 function getSecondsToday() {
-  var now = new Date();
-
-  // создать объект из текущей даты, без часов-минут-секунд
-  var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-
-  var diff = now - today; // разница в миллисекундах
-  return Math.floor(diff / 1000); // перевести в секунды
-}
-
-alert( getSecondsToday() );
+  var d = new Date();
+  return d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
+};
