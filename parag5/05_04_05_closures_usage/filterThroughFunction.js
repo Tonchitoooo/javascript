e93dbe0ea8a44c435b791b4f9b376be0ece4f60f
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-12 20:32:39 +0300

    Create filterThroughFunction.js

---
+++ parag5/05_04_05_closures_usage/filterThroughFunction.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <script src="https://learn.javascript.ru/test/libs.js"></script>
+  <script src="test.js"></script>
+</head>
+<body>
+
+  <script>
+
+    function filter(arr, func) {
+      var result = [];
+    
+      for (var i = 0; i < arr.length; i++) {
+        var val = arr[i];
+        if (func(val)) {
+          result.push(val);
+        }
+      }
+    
+      return result;
+    }
+    
+    function inArray(arr) {
+      return function(x) {
+        return arr.indexOf(x) != -1;
+      };
+    }
+    
+    function inBetween(a, b) {
+      return function(x) {
+        return x >= a && x <= b;
+      };
+    }
+
+  </script>
+
+</body>
+</html>
